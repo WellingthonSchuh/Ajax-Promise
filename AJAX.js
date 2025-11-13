76a029b2a80d9fa5b5334bf5f42cedf915c8b336
WellingthonSchuh <140724607+WellingthonSchuh@users.noreply.github.com>
2025-11-13 11:16:08 -0300

    add files

---
+++ AJAX.js
@@ -0,0 +1,60 @@
+// const request = obj => {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(obj.method, obj.url, true);
+//         xhr.send();
+
+//         xhr.addEventListener('load', () => {
+//             if(xhr.status >= 200 && xhr.status < 300) {
+//                 resolve(xhr.responseText);
+//             } else{
+//                 reject(xhr.statusText);
+//             }
+//         });
+//     });
+// };
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+})
+
+// async function carregaPagina(el){
+//     const href = el.getAttribute('href');
+//     try{
+//       const response = await request({
+//         method: 'GET',
+//         url: href
+//     });
+//     carregaResultado(response);  
+//     } catch(e) {
+//         console.log(e);
+//     }
+
+// }
+
+// as partes comentadas foram substituidas por esse fecth API
+async function carregaPagina(el) {
+    try{
+    const href = el.getAttribute('href');
+    const response = await fetch(href);
+
+    if(response.status !== 200) throw new Error('ERRO ao carregar a pagina');
+
+    const html = await response.text();
+    carregaResultado(html);
+    } catch(e){
+        console.error(e);
+    }
+}
+// até aqui.
+
+function carregaResultado(response){
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}
